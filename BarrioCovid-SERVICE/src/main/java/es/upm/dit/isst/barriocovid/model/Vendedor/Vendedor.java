d822186f63aa9ee3937227d1e29072df6ed63a0a
Szghuto09 <ed.chalva@gmail.com>
2021-04-08 18:13:18 +0200

    Primer commit de inicio para el proyecto BarrioCovid-SERVICE

---
+++ BarrioCovid-SERVICE/src/main/java/es/upm/dit/isst/barriocovid/model/Vendedor/Vendedor.java
@@ -0,0 +1,10 @@
+package es.upm.dit.isst.barriocovid.model.Vendedor;
+
+import java.io.Serializable;
+
+public class Vendedor implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+}
