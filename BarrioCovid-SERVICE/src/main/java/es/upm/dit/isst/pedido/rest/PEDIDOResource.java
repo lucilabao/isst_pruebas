d467956a3d181806d7ab1721d056bdcc1522f9a0
lucilabao <83296234+lucilabao@users.noreply.github.com>
2021-05-03 18:35:35 +0200

    cambios

--- BarrioCovid-SERVICE/src/main/java/es/upm/dit/isst/pedido/rest/PEDIDOResource.java
+++ BarrioCovid-SERVICE/src/main/java/es/upm/dit/isst/pedido/rest/PEDIDOResource.java
@@ -5,15 +5,18 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import es.upm.dit.isst.pedido.dao.PEDIDODAOImplementation;
 import es.upm.dit.isst.pedido.model.PEDIDO;
+
 
 @Path("/PEDIDOs")
 public class PEDIDOResource {
@@ -23,12 +26,18 @@ public class PEDIDOResource {
 		public List<PEDIDO> readAll () {
 			return PEDIDODAOImplementation.getInstance().readAll();
 		}
-		//@GET
-		//@Path("professor/{id}")
-		//@Produces(MediaType.APPLICATION_JSON)
-		//public List<PEDIDO> readAll (@PathParam("id") String id) {
-		//	return PEDIDODAOImplementation.getInstance().readAll(id);
-		//}
+		@GET
+		@Path("vendedor/{id}")
+		@Produces(MediaType.APPLICATION_JSON)
+		public List<PEDIDO> readAllVendedor (@PathParam("id") String id) {
+			return PEDIDODAOImplementation.getInstance().readAllVendedor(id);
+		}
+		@GET
+		@Path("voluntario/{id}")
+		@Produces(MediaType.APPLICATION_JSON)
+		public List<PEDIDO> readAllVoluntario (@PathParam("id") String id) {
+			return PEDIDODAOImplementation.getInstance().readAllVoluntario(id);
+		}
 		@POST
 		@Consumes(MediaType.APPLICATION_JSON)
 		public Response create(PEDIDO pnew) throws URISyntaxException {
@@ -39,5 +48,15 @@ public class PEDIDOResource {
 		    }
 			return Response.status(Response.Status.NOT_FOUND).build();
 		}
+		@DELETE
+		@Path("{id}")
+		public Response delete(@PathParam("id") String id) {
+			PEDIDO rold = PEDIDODAOImplementation.getInstance().read(id);
+			if (rold == null)
+				return Response.notModified().build();
+			PEDIDODAOImplementation.getInstance().delete(rold);
+			return Response.ok().build();
+		}
+
 }
 
