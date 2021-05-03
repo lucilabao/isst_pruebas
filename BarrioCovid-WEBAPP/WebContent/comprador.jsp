5b71549b89cc7baeccc3ee7b6ba24ce6b9d156ef
lucilabao <83296234+lucilabao@users.noreply.github.com>
2021-05-03 21:00:53 +0200

    primeras vistas y servlets

---
+++ BarrioCovid-WEBAPP/WebContent/comprador.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>COMPRADOR</title>
+</head>
+<body>
+	<form action="FormHacerPedido">
+		<button type="submit">Hacer Pedido</button>
+	</form>
+
+</body>
+<%@ include file="FormLogout.jsp"%>
+</html>
---
+++ BarrioCovid-WEBAPP/src/main/java/es/upm/dit/isst/pedido/servlets/ServletCompradores.java
@@ -0,0 +1,31 @@
+package es.upm.dit.isst.pedido.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ServletCompradores")
+public class ServletCompradores extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ServletCompradores() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
