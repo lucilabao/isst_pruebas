de2c8a4a2e96c4128a00ad37feebf16f1ef2bdd0
lucilabao <83296234+lucilabao@users.noreply.github.com>
2021-05-03 16:49:16 +0200

    covid service,modelo

---
+++ BarrioCovid-SERVICE/src/main/java/es/upm/dit/isst/pedido/model/PEDIDO.java
@@ -0,0 +1,51 @@
+package es.upm.dit.isst.pedido.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class PEDIDO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String comprador;
+	private String vendedor;
+	private String voluntario;
+	private int	id_pedido;
+	private String status;
+	
+	public String getComprador() {
+		return comprador;
+	}
+	public void setComprador(String comprador) {
+		this.comprador = comprador;
+	}
+	public String getVendedor() {
+		return vendedor;
+	}
+	public void setVendedor(String vendedor) {
+		this.vendedor = vendedor;
+	}
+	public String getVoluntario() {
+		return voluntario;
+	}
+	public void setVoluntario(String voluntario) {
+		this.voluntario = voluntario;
+	}
+	public int getId_pedido() {
+		return id_pedido;
+	}
+	public void setId_pedido(int id_pedido) {
+		this.id_pedido = id_pedido;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
