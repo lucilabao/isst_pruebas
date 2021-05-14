b2057ded924a0dbc7070654fea26c944f11dfb3e
luisdelgadoassiego7 <luisdelgadoassiego@gmail.com>
2021-05-14 13:22:15 +0200

    Commit reunión

--- BarrioCovid-SERVICE/src/main/java/es/upm/dit/isst/barriocovid/rest/PEDIDOResource.java
+++ BarrioCovid-SERVICE/src/main/java/es/upm/dit/isst/barriocovid/rest/PEDIDOResource.java
@@ -19,67 +19,67 @@ import es.upm.dit.isst.barriocovid.model.PEDIDO;
 
 @Path("/PEDIDOs")
 public class PEDIDOResource {
-		//PEDIDOs
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		public List<PEDIDO> readAll () {
-			return PEDIDODAOImplementation.getInstance().readAll();
+	//PEDIDOs
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<PEDIDO> readAll () {
+		return PEDIDODAOImplementation.getInstance().readAll();
+	}
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response create(PEDIDO pnew) throws URISyntaxException {
+		PEDIDO p = PEDIDODAOImplementation.getInstance().create(pnew);
+		if (p != null) {
+			URI uri = new URI("/BarrioCovid-SERVICE/rest/PEDIDOs/" + p.getComprador());
+			return Response.created(uri).build();
 		}
-		@POST
-		@Consumes(MediaType.APPLICATION_JSON)
-		public Response create(PEDIDO pnew) throws URISyntaxException {
-			PEDIDO p = PEDIDODAOImplementation.getInstance().create(pnew);
-			if (p != null) {
-				URI uri = new URI("/BarrioCovid-SERVICE/rest/PEDIDOs/" + p.getComprador());
-					return Response.created(uri).build();
-		    }
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
+	//PEDIDOs/id
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response read(@PathParam("id") String id) {
+		PEDIDO p = PEDIDODAOImplementation.getInstance().read(id);
+		if (p == null)
 			return Response.status(Response.Status.NOT_FOUND).build();
-		}
-		//PEDIDOs/id
-		@GET
-		@Path("{id}")
-		@Produces(MediaType.APPLICATION_JSON)
-		public Response read(@PathParam("id") String id) {
-			PEDIDO p = PEDIDODAOImplementation.getInstance().read(id);
-			if (p == null)
-				return Response.status(Response.Status.NOT_FOUND).build();
-			return Response.ok(p, MediaType.APPLICATION_JSON).build();
-		}
-		@POST
-		@Consumes(MediaType.APPLICATION_JSON)
-		@Path("{id}")
-		public Response update(@PathParam("id") String id, PEDIDO p) {
-			System.out.println("Update request for" + id + " " +
-					p.toString());
-			PEDIDO told = PEDIDODAOImplementation.getInstance().read(id);
-			if ((told == null) || (!
-					told.getComprador().contentEquals(p.getComprador())))
-				return Response.notModified().build();
-			PEDIDODAOImplementation.getInstance().update(p); return Response.ok().build(); 
-		}
-		@DELETE
-		@Path("{id}")
-		public Response delete(@PathParam("id") String id) {
-			PEDIDO rold = PEDIDODAOImplementation.getInstance().read(id);
-			if (rold == null)
-				return Response.notModified().build();
-			PEDIDODAOImplementation.getInstance().delete(rold);
-			return Response.ok().build();
-		}
-		//PEDIDOs/vendedor/id
-		@GET
-		@Path("vendedor/{id}")
-		@Produces(MediaType.APPLICATION_JSON)
-		public List<PEDIDO> readAllVendedor (@PathParam("id") String id) {
-			return PEDIDODAOImplementation.getInstance().readAllVendedor(id);
-		}
-		//PEDIDOs/voluntario/id
-		@GET
-		@Path("voluntario/{id}")
-		@Produces(MediaType.APPLICATION_JSON)
-		public List<PEDIDO> readAllVoluntario (@PathParam("id") String id) {
-			return PEDIDODAOImplementation.getInstance().readAllVoluntario(id);
-		}
+		return Response.ok(p, MediaType.APPLICATION_JSON).build();
+	}
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public Response update(@PathParam("id") String id, PEDIDO p) {
+		System.out.println("Update request for" + id + " " +
+				p.toString());
+		PEDIDO told = PEDIDODAOImplementation.getInstance().read(id);
+		if ((told == null) || (!
+				told.getComprador().contentEquals(p.getComprador())))
+			return Response.notModified().build();
+		PEDIDODAOImplementation.getInstance().update(p); return Response.ok().build(); 
+	}
+	@DELETE
+	@Path("{id}")
+	public Response delete(@PathParam("id") String id) {
+		PEDIDO rold = PEDIDODAOImplementation.getInstance().read(id);
+		if (rold == null)
+			return Response.notModified().build();
+		PEDIDODAOImplementation.getInstance().delete(rold);
+		return Response.ok().build();
+	}
+	//PEDIDOs/vendedor/id
+	@GET
+	@Path("vendedor/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<PEDIDO> readAllVendedor (@PathParam("id") String id) {
+		return PEDIDODAOImplementation.getInstance().readAllVendedor(id);
+	}
+	//PEDIDOs/voluntario/id
+	@GET
+	@Path("voluntario/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<PEDIDO> readAllVoluntario (@PathParam("id") String id) {
+		return PEDIDODAOImplementation.getInstance().readAllVoluntario(id);
+	}
 
 
 }
